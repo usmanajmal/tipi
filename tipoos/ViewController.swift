01314bdcf8a24c5293d5900af49787864947363f
Usman Ajmal <syedusmanajmal@gmail.com>
2016-09-03 10:21:31 -0700

    First cut of Tipi
    - Add bill
    - Dynamically calculate tip on adding bill
    - Add tip percentage controller
    - Dynamically change tip on changing tip %age
    
    Signed-off-by: Usman Ajmal <syedusmanajmal@gmail.com>

--- tipoos/ViewController.swift
+++ tipoos/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+     *  Dismiss keyboard on tapping main View
+     */
+    @IBAction func onTapMainView(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    /**
+     *  Dynamically set tip
+     */
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.15, 0.20, 0.25]
+        
+        // Get a valid string and convert to number
+        // If input string is not a valid number show 0
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipSegment.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Version 1
+        //tipLabel.text = String(tip)
+        //totalLabel.text = String(total)
+        
+        // Version 2
+        //tipLabel.text = "$\(tip)"
+        //totalLabel.text = "$\(total)"
+        
+        // Version 3
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
